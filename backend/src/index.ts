b7c911e468d508ad0f04be01dbea537ea07573ae
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-15 15:46:37 +0530

    adding acceltarion url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
-// neon db url postgresql://first_owner:3PQDV6umNjst@ep-late-snow-a5o89d38.us-east-2.aws.neon.tech/datab?sslmode=require
+//postgresql://first_owner:3PQDV6umNjst@ep-late-snow-a5o89d38.us-east-2.aws.neon.tech/datab?sslmode=require
 
 
-// prisma accelrate key DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiOGU4Y2U4NWYtMGFiYy00NDMxLTljYTItYjJiNWM5ZjRmYTljIiwidGVuYW50X2lkIjoiOTllNWViMTFhNjNkZWJkODZjNjExM2UyYzZjOGZkY2FhMWNiMTdiNzU5ZTBhNjlkNGZiNmRhYTE0OTAxNmUwZCIsImludGVybmFsX3NlY3JldCI6IjhkYjVlZDAzLTA4MWYtNGYxNS05ZjAxLThkNjY5YjM1Zjc4OSJ9.ssz7GBmUV_AuIPVjrulYuXVPrXATJuT01p0nfU6d5Gg"
+// DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiOGU4Y2U4NWYtMGFiYy00NDMxLTljYTItYjJiNWM5ZjRmYTljIiwidGVuYW50X2lkIjoiOTllNWViMTFhNjNkZWJkODZjNjExM2UyYzZjOGZkY2FhMWNiMTdiNzU5ZTBhNjlkNGZiNmRhYTE0OTAxNmUwZCIsImludGVybmFsX3NlY3JldCI6IjhkYjVlZDAzLTA4MWYtNGYxNS05ZjAxLThkNjY5YjM1Zjc4OSJ9.ssz7GBmUV_AuIPVjrulYuXVPrXATJuT01p0nfU6d5Gg"
