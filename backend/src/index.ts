b8c69119224bf56aa67671893a36922906376e85
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-15 11:50:21 +0530

    backend routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,19 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/api/v1/signup', (c) => {
+  return c.text('Hello Hono. its signoup!')
+})
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
