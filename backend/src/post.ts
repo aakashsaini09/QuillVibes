58a4468f06af7b507bedfcc1ed1ffca9b56c89b2
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-17 19:20:42 +0530

    addnew post

--- backend/src/post.ts
+++ backend/src/post.ts
@@ -28,3 +28,18 @@ bookRouter.use(async (c, next) => {
 	c.set('userId', payload.id);
 	await next()
 });
+bookRouter.use(async (c, next) => {
+    const jwt = c.req.header('Authorization');
+	if (!jwt) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	const token = jwt.split(' ')[1];
+	const payload = await verify(token, c.env.JWT_SECRET);
+	if (!payload) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	c.set('userId', payload.id);
+	await next()
+});
