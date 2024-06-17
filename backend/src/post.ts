017adcbbbc132ff637d5b92054c5dda68b5eeb80
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-17 19:21:15 +0530

    new route

--- backend/src/post.ts
+++ backend/src/post.ts
@@ -43,3 +43,23 @@ bookRouter.use(async (c, next) => {
 	c.set('userId', payload.id);
 	await next()
 });
+
+
+bookRouter.post('/', async (c) => {
+	const userId = c.get('userId');
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const post = await prisma.post.create({
+		data: {
+			title: body.title,
+			content: body.content,
+			authorId: userId
+		}
+	});
+	return c.json({
+		id: post.id
+	});
+})
