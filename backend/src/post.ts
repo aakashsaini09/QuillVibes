de74ab9ff75e7dcfb7e00d8827ce0a528b9135f3
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-17 19:20:07 +0530

    post

---
+++ backend/src/post.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { verify } from "hono/jwt";
+
+export const bookRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    },
+    Variables: {
+        userId: string
+    }
+}>();
+
+bookRouter.use(async (c, next) => {
+    const jwt = c.req.header('Authorization');
+	if (!jwt) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	const token = jwt.split(' ')[1];
+	const payload = await verify(token, c.env.JWT_SECRET);
+	if (!payload) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	c.set('userId', payload.id);
+	await next()
+});
