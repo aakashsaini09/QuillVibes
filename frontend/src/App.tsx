217849e60d48e042f1ecc75442cc35e877a7942a
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-21 22:32:52 +0530

    frontend routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Blog from './pages/Blog'
 function App() {
   return (
     <>
-      <p className="text-blue-800">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
