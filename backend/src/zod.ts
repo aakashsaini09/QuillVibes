490675d0ed92fcf6dd2adf48e1905703cfc21a7e
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-21 11:41:09 +0530

    common

--- backend/src/zod.ts
+++
@@ -1,7 +0,0 @@
-import z from 'zod'
-export const signupInput = z.object({
-    username: z.string().email(),
-    password: z.string().min(6),
-    name: z.string().optional()
-})
-export type SignupInput = z.infer<typeof signupInput>
