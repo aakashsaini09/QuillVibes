b93fb400de14e5730ef500b7aa242f3eebf59a71
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-24 18:49:17 +0530

    delete blog api

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -92,6 +92,33 @@ blogRoute.post('/', async(c) => {
             id: blog.id
         })
     })
+    // *********************************************delete blog********************************************
+    // blogRoute.delete('/', async(c) => {
+    //     const prisma = new PrismaClient({
+    //         datasourceUrl: c.env.DATABASE_URL,
+    //     }).$extends(withAccelerate())
+        
+    //     const body = await c.req.json()
+    //     const { success } = updateBlogInput.safeParse(body)
+    //     if(!success){
+    //         c.status(411)
+    //         return c.json({
+    //             message: "Input is not correct"
+    //         })
+    //     }
+    //     const blog = await prisma.blog.update({
+    //         where: {
+    //             id: body.id
+    //         },
+    //         data: {
+    //             title: body.title,
+    //             content: body.content,
+    //         }
+    //     });
+    //     return c.json({
+    //         id: blog.id
+    //     })
+    // })
         // *********************************************get all the blogs********************************************
         blogRoute.get('/bulk', async (c) => {
             const prisma = new PrismaClient({
