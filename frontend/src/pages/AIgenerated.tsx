b5a2d86675789293258070cd4ea02de3ab521308
aakashsaini09 <aakashsaini948585@gmail.com>
2024-07-30 14:24:52 +0530

    bug fixed

--- frontend/src/pages/AIgenerated.tsx
+++ frontend/src/pages/AIgenerated.tsx
@@ -38,6 +38,10 @@ const AIgenerated = () => {
 
 
   const AIRequest = async(prompt: string) => {
+    if(prompt.length < 5){
+      alert("Title is too short. Please enter a title with atleast 5 characters")
+      return null;
+    }
     try{
     const response = await axios.post(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_Key}`, 
       {contents: [ {
@@ -61,12 +65,10 @@ catch (error) {
     return "";
 }
 };
-
-
-
+const [checking, setchecking] = useState(false)
     const postBlog = async() =>{
       setloading(true)
-      // if(title.length >= 5 && content.length>= 10){
+      if(title.length >= 5 && content.length>= 10){
         const res = await AIRequest(title)
         setcontent(res)
         console.log("response is:::::",res)
@@ -80,15 +82,41 @@ catch (error) {
                Authorization: localStorage.getItem("token")}});
                setloading(false)
                navigate(`/blog/${response.data.id}`)
-      // }else{
-      //   setloading(false)
-      //     alert("Content is small. Add more details please")
-      //   }
+      }else{
+        checkingFunction()
+        setloading(false)
+        }
       }
+      function checkingFunction() {
+        setchecking(true);
+        setTimeout(() => {
+          setchecking(false);
+        }, 6000);
+      }
+      // useEffect(() => {
+      //   setTimeout(() => {
+      //     setchecking(false);
+      //   }, 6000);
+      // }, [checking])
+      
   return (
     <>
   <Navbar/>
-  <div className="flex justify-center w-full px-2">
+  <div className="flex flex-col justify-center w-full px-2 items-center">
+
+  { checking? <div className="flex items-center w-full p-4 rounded-lg bg-gray-500">
+      <div className="ms-3 text-sm font-medium text-white">
+        Title Should be atleast 5 characters long!
+      </div>
+      <button type="button" onClick={() =>setchecking(false)} className="ms-auto -mx-1.5 -my-1.5 text-gray-50 bg-gray-500 rounded-lg p-1.5 hover:bg-gray-400 inline-flex items-center justify-center h-8 w-8">
+        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+        </svg>
+      </button>
+    </div>
+: ""}
+
+
     <div className="max-w-screen-xl w-full">
     <input onChange={(e) => { settitle(e.target.value)}} type="text" className="bg-gray-100 outline-none text-gray-900 text-base rounded-lg block w-full p-3 pl-5 py-7 mt-5 mb-14" placeholder="Title" required  />
     <button disabled={loading ? true : false} onClick={postBlog} type="submit" className={`mt-4 inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg ${loading ? 'cursor-not-allowed' : ''}`}>{loading ? "Generating" : "Generate"}</button>
