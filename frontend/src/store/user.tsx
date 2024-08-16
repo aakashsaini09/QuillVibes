c65420fe3e42a9636f87732945f3a17bca64ef88
Aakash Saini <121600651+aakashsaini09@users.noreply.github.com>
2024-08-16 18:33:38 +0530

    Update user.tsx

--- frontend/src/store/user.tsx
+++ frontend/src/store/user.tsx
@@ -3,3 +3,5 @@ export const UserName = atom({
     key: "UserName",
     default: "Aakash Saini"
 })
+// https://api.github.com/repositories/815427272/contributors
+// https://api.github.com/repos/aakashsaini09/QuillVibes
